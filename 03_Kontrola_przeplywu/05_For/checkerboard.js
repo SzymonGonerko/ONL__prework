7677d6ebf8686c3a977c49b11bb52e6ea0b66e0f
SzymonGonerko <szymon.gonerko@gmail.com>
2022-06-13 23:57:34 +0200

    For were done

--- 03_Kontrola_przeplywu/05_For/checkerboard.js
+++ 03_Kontrola_przeplywu/05_For/checkerboard.js
@@ -6,10 +6,22 @@ console.log(15 % 2);
 console.log(10 % 2);
 
 function generateCheckerboard(times, blackTileChar, whiteTileChar) {
+    for (let i = 0; i < times; i++) {
+        let chessboard = [];
+        for (let j = 0; j < times; j++) {
+            if ( (i + j) % 2 === 0) {
+                chessboard.push(blackTileChar)
+            } else {
+                chessboard.push(whiteTileChar)
+            }
+        }
+        console.log(chessboard.join(""))
+    }
+
 }
 
 console.log('\n/**************/');
 console.log('| CHECKERBOARD |');
 console.log('/**************/');
 
-generateCheckerboard(8, '##', '__');
+generateCheckerboard(30, '#', '_');
