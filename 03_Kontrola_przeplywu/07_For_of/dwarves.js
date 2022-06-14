0df1050329a8a989011e5054b24ce9345af59136
SzymonGonerko <szymon.gonerko@gmail.com>
2022-06-14 20:50:13 +0200

    dwarves were done

--- 03_Kontrola_przeplywu/07_For_of/dwarves.js
+++ 03_Kontrola_przeplywu/07_For_of/dwarves.js
@@ -3,7 +3,16 @@ const dwarves = ['Dopey', 'Sneezy', 'Bashful', 'Doc', 'Happy', 'Grumpy', 'Sleepy
 function hailDwarves(dwarfNames) {
   // To jest fraza początkowa. Nie zmieniaj jej!
   let phrase = 'Dinner is ready! Come home ';
+  for (let [key, value] of dwarves.entries()) {
+    phrase +=  value;
+   
+    key === dwarves.length -1 || key === dwarves.length -2 ? null :  phrase += ", "
+    key === dwarves.length -2 ? phrase += " and " : null
+  }
 
+phrase += "!"
+
+  console.log(phrase)
   // Tutaj umieść swoją pętlę
 
   return phrase;
