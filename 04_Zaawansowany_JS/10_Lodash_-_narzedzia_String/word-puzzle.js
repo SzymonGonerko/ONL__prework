4bcbdb54350b819a36e0a432a6d21a43e6717295
SzymonGonerko <szymon.gonerko@gmail.com>
2022-06-17 14:59:24 +0200

    word-puzzle were done

--- 04_Zaawansowany_JS/10_Lodash_-_narzedzia_String/word-puzzle.js
+++ 04_Zaawansowany_JS/10_Lodash_-_narzedzia_String/word-puzzle.js
@@ -57,7 +57,7 @@ const objD = {
 
 // Change merge order in a way that merged object consists of ALL THE LETTERS needed for creating "INCEPCJA"
 // Current order - objA, objB, objC, objD - is a default one and does not guarantee ability to get all the letters
-const mergedObj = _.merge(objA, objB, objC, objD);
+const mergedObj = _.merge(objC, objB, objA, objD);
 
 // This is somewhat a hint - here you can see which letters are used to build "INCEPCJA"
 const letterGetters = [
@@ -76,6 +76,12 @@ const dashRegEx = /-/ig;
 const properLetterGetters = letterGetters.map(function (letterPath) {
   // HERE - use one of case-functions (e.g. `_.toXCase()` to properly transform letter path into dash-separated one
   // Also - use replace with "dashRegEx" to replace all the dashes with dots
+
+  letterPath = _.kebabCase(letterPath)
+  
+  letterPath = letterPath.replace(dashRegEx, ".")
+
+  console.log(letterPath)
 
   // Provide proper return value once you modify the function - it should return letter path looking like "a.b.c"
   return letterPath;
