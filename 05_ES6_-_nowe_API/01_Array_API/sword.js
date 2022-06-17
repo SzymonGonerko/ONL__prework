bec72b4c052d2f564bbbfb85e70f31c057b74acd
SzymonGonerko <szymon.gonerko@gmail.com>
2022-06-17 21:07:27 +0200

    sword were done

--- 05_ES6_-_nowe_API/01_Array_API/sword.js
+++ 05_ES6_-_nowe_API/01_Array_API/sword.js
@@ -7,12 +7,22 @@ const SWORD_PATTERN = '{}oo((X))>';
 function sword(length) {
   // First = create array from sword pattern
 
+  const sword = Array.from(SWORD_PATTERN)
+
   // Second - create two arrays from sword pattern array
+
+  const handle = sword.slice(0, sword.length -1)
+  const vertex = sword.slice(sword.length -1, sword.length)
 
   // Third = create blade pattern, using `Array(x).fill(''); with proper length and character
 
+  const blade = Array(length).fill(BLADE_CHARACTER)
+
   // Fourth - concatenate the arrays and join with empty character, using `join('')`
-  return SWORD_PATTERN;
+
+  const holeWeapon = [...handle, ...blade, ...vertex].join("")
+
+  return holeWeapon;
 }
 
 const shortSword = sword(5);
